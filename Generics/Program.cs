ce22ae0814f5aa1d8709d556f8b27c60a89bf67f
bernarth <alberthbernarth75@gmail.com>
2024-04-09 08:37:23 -0400

    Add initial Generics project

---
+++ Generics/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Generics;
+
+class Program
+{
+    static void Main()
+    {
+        // Test arrays
+        int[] intArray = { 1, 2, 3, 4, 5 };
+        double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+        string[] stringArray = { "Apple", "Banana", "Cherry", "Date" };
+
+        // Testing FindMaximum
+
+        // Testing FindMinimum
+
+        // Testing ContainsValue
+        
+    }
+}
