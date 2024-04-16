f94f182565b7ed2aedbe4a308abb2096e7194028
Ariel.Rojas <arielwagnerrojas@gmail.com>
2024-04-16 16:21:56 -0400

    feat: implement code to get the greatest common divisor

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Recursion
+{
+    public class Program
+    {
+
+        /// <summary>
+        /// Gets the greatest common divisor for two numbers.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <returns></returns>
+        public static int GetGreatestCommonDivisor(int firstNumber, int secondNumber)
+        {
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+            else
+            {
+                return GetGreatestCommonDivisor(secondNumber, firstNumber % secondNumber);
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine($"The greatest common divisor of 156 and 120 is: {GetGreatestCommonDivisor(156, 120)}");
+            Console.WriteLine($"The greatest common divisor of 100 and 60 is: {GetGreatestCommonDivisor(100, 60)}");
+        }
+    }
+}
