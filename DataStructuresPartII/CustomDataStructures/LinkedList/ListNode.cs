7a5bacd9cb134fb6a5d0656e5b23ff1310f6ab08
Angel Cachi <angel.cachi@jalasoft.com>
2024-04-17 01:22:57 -0400

    fuctions implemented-Task3/data structure

--- DataStructuresPartII/CustomDataStructures/LinkedList/ListNode.cs
+++ DataStructuresPartII/CustomDataStructures/LinkedList/ListNode.cs
@@ -5,10 +5,12 @@ public class ListNode<T>
     public T Data { get; set; }
 
     public ListNode<T> Next { get; set; }
+    public ListNode<T> Previous { get; set; }
 
     public ListNode(T data)
     {
         Data = data;
         Next = null;
+        Previous = null; 
     }
 }
