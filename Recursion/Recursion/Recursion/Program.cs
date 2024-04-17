40a0162e96043380024ac75a572558e38278339e
Angel Cachi <angel.cachi@jalasoft.com>
2024-04-17 02:06:10 -0400

    implementing feature get GCD

---
+++ Recursion/Recursion/Recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Recursion;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 120;
+        int num2 = 156;
+
+        Console.WriteLine($"The Greatest Common Divisor (GCD) of ({num1} and {num2}) are: {GetGreatestCommonDivisor(num1, num2)}");
+    }
+
+    public static int GetGreatestCommonDivisor(int num1, int num2)
+    {  
+        if (num2 > num1)
+        {
+            int aux = num1;
+            num1 = num2;
+            num2 = aux;
+        }
+        
+        if (num2 == 0)
+        {
+            return num1;
+        }
+        else 
+        {
+            return GetGreatestCommonDivisor(num2, num1%num2);
+        }        
+    }
+}
