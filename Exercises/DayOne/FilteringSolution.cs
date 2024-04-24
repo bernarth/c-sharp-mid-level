9ce5f7f3dc85b76b67fb4ac0fc61ad60c650aa7c
bernarth <alberthbernarth75@gmail.com>
2024-04-24 08:02:49 -0400

    add exercises for day one practice filtering and trees

---
+++ Exercises/DayOne/FilteringSolution.cs
@@ -0,0 +1,14 @@
+﻿namespace DayOne;
+
+using System.Collections.Generic;
+
+public class FilteringSolution
+{
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        object test = 12;
+        Console.WriteLine(test.GetType() == typeof(int));
+
+        return new List<int>();
+    }
+}
--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -6,7 +6,18 @@ public class Program
 {
     static void Main()
     {
-        var result = Template.NumericFormatter("xxxx yyyy zzzz");
-        Console.WriteLine(result);
+        //var result = Template.NumericFormatter("xxxx yyyy zzzz");
+        //Console.WriteLine(result);
+
+        //var tree = new TreeNode(17,
+        //    new TreeNode(3, 
+        //        new TreeNode(2), null), 
+        //    new TreeNode(-10, 
+        //        new TreeNode(16), new TreeNode(1, 
+        //            new TreeNode(13))));
+        //var result = TreeSolution.MaxSum(tree);
+        //Console.WriteLine(result);
+
+        FilteringSolution.GetIntegersFromList(new List<object>());
     }
 }
