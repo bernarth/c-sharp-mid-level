8e1b0a18b8e326668c4ebdcfe60fc592c1c15ba3
bernarth <alberthbernarth75@gmail.com>
2024-04-24 07:06:21 -0400

    add: add initial folder structure for exercises day one

---
+++ Exercises/DayOne/Template.cs
@@ -0,0 +1,28 @@
+﻿namespace DayOne;
+
+public class Template
+{
+    public const string DefaultNumber = "1234567890";
+
+    public static string NumericFormatter(string template, string number = DefaultNumber)
+    {
+        var numberFormatted = string.Empty;
+        var position = 0;
+        var maxPosition = number.Length;
+
+        foreach (var character in template)
+        {
+            if (char.IsLetter(character))
+            {
+                numberFormatted += number[position];
+                position = (position + 1) % maxPosition;
+            }
+            else
+            {
+                numberFormatted += character;
+            }
+        }
+
+        return numberFormatted;
+    }
+}
