22a50c4d3bbcb8dcb0e9b9ea32a034fb6ddcf86a
Ariel.Rojas <arielwagnerrojas@gmail.com>
2024-04-24 08:31:48 -0400

    feat: implement method to get integers from list

--- Exercises/DayOne/FilteringSolution.cs
+++ Exercises/DayOne/FilteringSolution.cs
@@ -6,9 +6,22 @@ public class FilteringSolution
 {
     public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
     {
-        object test = 12;
-        Console.WriteLine(test.GetType() == typeof(int));
 
-        return new List<int>();
+        var result = new List<int>();
+
+        foreach (object item in listOfItems)
+        {
+            if (item.GetType() == typeof(int))
+            {
+                result.Add((int)item);
+            }
+        }
+
+        return result;
+    }
+
+    public static void PrintList(IEnumerable<int> list)
+    {
+        Console.WriteLine(string.Join(",", list));
     }
 }
--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -6,18 +6,22 @@ public class Program
 {
     static void Main()
     {
-        //var result = Template.NumericFormatter("xxxx yyyy zzzz");
-        //Console.WriteLine(result);
+        var result = Template.NumericFormatter("xxxx yyyy zzzz");
+        Console.WriteLine(result);
 
-        //var tree = new TreeNode(17,
-        //    new TreeNode(3, 
-        //        new TreeNode(2), null), 
-        //    new TreeNode(-10, 
-        //        new TreeNode(16), new TreeNode(1, 
-        //            new TreeNode(13))));
-        //var result = TreeSolution.MaxSum(tree);
-        //Console.WriteLine(result);
+        var tree = new TreeNode(17,
+            new TreeNode(3,
+                new TreeNode(2), null),
+            new TreeNode(-10,
+                new TreeNode(16), new TreeNode(1,
+                    new TreeNode(13))));
+        var resultTree = TreeSolution.MaxSum(tree);
+        Console.WriteLine(resultTree);
 
-        FilteringSolution.GetIntegersFromList(new List<object>());
+        var list = new List<object>() { 1, 2, "a", "b" };
+        var listInteger = FilteringSolution.GetIntegersFromList(list);
+
+        Console.Write("Get Integers From List: ");
+        FilteringSolution.PrintList(listInteger);
     }
 }
