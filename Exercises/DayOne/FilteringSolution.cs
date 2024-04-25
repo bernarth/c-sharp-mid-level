a7e3a925f01f744ff6ccc821d8e26234887101da
Angel Cachi <angel.cachi@jalasoft.com>
2024-04-25 04:54:07 -0400

    task-resolving katas

--- Exercises/DayOne/FilteringSolution.cs
+++ Exercises/DayOne/FilteringSolution.cs
@@ -4,11 +4,28 @@ using System.Collections.Generic;
 
 public class FilteringSolution
 {
-    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
-    {
-        object test = 12;
-        Console.WriteLine(test.GetType() == typeof(int));
+    public static List<int> GetIntegersFromList(List<object> listOfItems)
+    {        
+        List<int> result = new List<int>();
 
-        return new List<int>();
+        foreach (object item in listOfItems)
+        {
+            if (item is int)
+            {
+                int number = (int)item;
+                if (number >= 0)
+                {
+                    result.Add(number);
+                }                
+            }
+        }        
+
+        return result;
+    }
+
+    public static void PrintList(List<int> list)
+    {
+        string result = string.Join(", ", list);
+        Console.WriteLine(result);
     }
 }
---
+++ Exercises/DayOne/RemoveElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOne
+{
+    public class RemoveElements
+    {
+        public static int[] Remove(int[] elements, int[] remove) 
+        {
+            List<int> result = new List<int>();                        
+            
+            foreach (int element in elements)
+            {                    
+                if ( !remove.Contains(element))
+                {
+                    result.Add(element);
+                }                    
+            }
+          
+            return result.ToArray();            
+        }
+    }
+}
