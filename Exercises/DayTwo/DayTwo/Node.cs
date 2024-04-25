d693fc21e6b3ba5dad2340aad0ba60276dcc0589
Ariel.Rojas <arielwagnerrojas@gmail.com>
2024-04-25 08:32:53 -0400

    feat: implement Linked list Get Nth Node

--- Exercises/DayTwo/DayTwo/Node.cs
+++ Exercises/DayTwo/DayTwo/Node.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +20,39 @@ namespace DayTwo
 
         public static Node GetNth(Node node, int index)
         {
-            throw new NotImplementedException();
+            int count = 0;
+
+            if (node == null || index > CountNodes(node))
+            {
+                throw new ArgumentException();
+            }
+
+            while(node != null)
+            {
+                if (count == index)
+                {
+                    return node;
+                }
+
+                node = node.Next;
+                count++;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public static int CountNodes(Node node)
+        {
+            int count = 0;
+
+            while (node != null)
+            {
+                count++;
+                node = node.Next;
+                
+            }
+
+            return count;
         }
     }
 }
