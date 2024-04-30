60fc02b50ff13002a3a43c7531f0b493ab93f6e5
Angel Cachi <angel.cachi@jalasoft.com>
2024-04-29 22:20:33 -0400

    solving kata recursive and without recursive

--- Exercises/DayTwo/DayTwo/Program.cs
+++ Exercises/DayTwo/DayTwo/Program.cs
@@ -7,6 +7,12 @@
             TreeNode root = TreeNode.Leaf().WithLeaves();
             Console.WriteLine(TreeNode.IsPerfect(root));
 
+            Console.WriteLine("Kata Recursive");
+            int numberFactorial = 6;
+            Console.WriteLine($"The factorial of number {numberFactorial} is: {Recursion.FacCalculation(numberFactorial)}");
+
+            Console.WriteLine("----Without Recursive");
+            Console.WriteLine($"The factorial of number {numberFactorial} is:{Recursion.CalculateFactorial(numberFactorial)}");
         }
     }
 }
